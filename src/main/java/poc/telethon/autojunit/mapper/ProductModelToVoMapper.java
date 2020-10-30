bc42d442f7bdd19f97f173690cae6d56fa35d30a
Retheesh <retheeshm_r@infosys.com>
2020-10-30 12:07:14 +0100

    Added one method

--- src/main/java/poc/telethon/autojunit/mapper/ProductModelToVoMapper.java
+++ src/main/java/poc/telethon/autojunit/mapper/ProductModelToVoMapper.java
@@ -12,9 +12,19 @@ public class ProductModelToVoMapper {
     public ProductVo map(ProductModel productModel) {
         ProductVo productVo = ProductVo.builder()
                 .productId(productModel.getProductId())
-                .productName(productModel.getProductName())
+                .productName(mapProductName(productModel.getProductName()))
                 .productStatus(productModel.getProductStatus())
                 .build();
         return productVo;
+    }
+
+    private String mapProductName(String productNameModel) {
+        if(productNameModel.equalsIgnoreCase("F")) {
+            return "Fixed";
+        } else if(productNameModel.equalsIgnoreCase("M")) {
+            return "Mobile";
+        } else {
+            return "NA";
+        }
     }
 }
